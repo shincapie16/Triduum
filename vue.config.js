840aa05ad26e999c872d751fe687c6842626bb72
shincapie16 <santiago.hincapie@utp.edu.co>
2023-04-03 14:15:39 -0500

    Configurar Dominio

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Triduum/'
+    : '/'
 }
+
 )
